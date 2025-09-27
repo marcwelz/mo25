e584553dd8d8225e3d29cc18119609fc56926281
marcwelz Private <marc.welz03@gmail.com>
2025-09-27 11:00:04 +0200

    update exercise

--- helper_functions.py
+++ helper_functions.py
@@ -5,15 +5,20 @@ Created on 20.09.2025 11:28
 @author: marcwelz
 @project: mo25
 """
+from typing import Optional
 
-def read_float(prompt: str, default: float) -> float:
+def read_float(prompt: str, default: Optional[float] = None) -> float:
+    if default is None:
+        default = 10
     try:
         return float(input(prompt))
     except ValueError:
         print(f"invalid input, us default value {default}")
         return default
 
-def read_int(prompt: str, default: int) -> int:
+def read_int(prompt: str, default: Optional[int] = None) -> int:
+    if default is None:
+        default = 10
     try:
         return int(input(prompt))
     except ValueError:
--- sw1/exercises/braking_distance.py
+++ sw1/exercises/braking_distance.py
@@ -8,10 +8,12 @@ Created on 20.09.2025 10:34
 import sys
 from typing import Final
 
+from helper_functions import read_float
+
 FRICTION: Final[float] = 0.3
 GRAVITY_ACCELERATION: Final[float] = 9.81
 
-velocity: float = float(sys.argv[1])
+velocity: float = read_float(sys.argv[1])
 
 def calculate_break_distance(v: float, f: float, g: float) -> float:
     return ((v ** 2) / 2) / (f * g)
