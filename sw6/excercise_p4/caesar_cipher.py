f069883b29a0e29ca644fb09b737e9ab9cee883f
marcwelz Private <marc.welz03@gmail.com>
2025-11-01 11:21:04 +0100

    init excercises and improvments

--- sw6/excercise_p4/caesar_cipher.py
+++ sw6/excercise_p4/caesar_cipher.py
@@ -11,29 +11,36 @@ for i in range(ord('a'), ord('z') + 1):
     alphabet.append(chr(i))
 alphabet_length: int = len(alphabet)
 
-def encript(text: str) -> str:
-    chars:list[str] = list(text)
+def encrypt(text: str) -> str:
+    chars:list[str] = list(text.lower())
     new_chars:list[str] = []
 
     for char in chars:
-        index_of_char = alphabet.index(char)
-        new_chars.append(alphabet[(index_of_char - 3) % alphabet_length])
+        if char in alphabet:
+            index_of_char = alphabet.index(char)
+            new_chars.append(alphabet[(index_of_char - 3) % alphabet_length])
+        else:
+            new_chars.append(char)
 
     return ''.join(new_chars)
 
-def decript(text: str) -> str:
+def decrypt(text: str) -> str:
     chars:list[str] = list(text)
     new_chars:list[str] = []
 
     for char in chars:
-        index_of_char = alphabet.index(char)
-        new_chars.append(alphabet[(index_of_char + 3) % alphabet_length])
+        if char in alphabet:
+            index_of_char = alphabet.index(char)
+            new_chars.append(alphabet[(index_of_char + 3) % alphabet_length])
+        else:
+            new_chars.append(char)
+
 
     return ''.join(new_chars)
 
 user_input: str = input("Enter your message: ")
 
-encripted_message: str = encript(user_input)
+encripted_message: str = encrypt(user_input)
 
 print(encripted_message)
-print(decript(encripted_message))
+print(decrypt(encripted_message))
---
+++ sw6/excercise_p4/credit_card_number_check.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 01.11.2025 10:10
+@author: marcwelz
+@project: mo25
+"""
+
+credit_card_number_input:str = input("Enter the credit card number: XXXX XXXX XXXX XXXX")
+
+def check_if_credit_card_number_is_valid(credit_card_number:str) -> bool:
+    if len(credit_card_number) != 19:
+        return False
+
+    chars:list[str] = list(credit_card_number)
+
+    for index, char in enumerate(chars):
+        if index == 4 or index == 9 or index == 14 or index == 19:
+            if char == ' ':
+                continue
+            else:
+                return False
+        else:
+            if char.isdigit():
+                continue
+            else:
+                return False
+
+    sum_total: int = 0
+    for section in credit_card_number.split(' '):
+        for number in section:
+            sum_total += int(number)
+
+    return sum_total % 10 == 0
+
+print(check_if_credit_card_number_is_valid(credit_card_number_input))
